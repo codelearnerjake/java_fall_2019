4ec618736b72df1a2303d95412050dfa5167d5eb
Jacob Borchers <destinybound92@gmail.com>
2019-11-07 17:53:45 -0500

    Method practice 2 (Sammy rental 4)

---
+++ JBorchers_Prog11/src/jBorchers_Prog11/SammyRentals4.java
@@ -0,0 +1,16 @@
+/* Jacob Borchers
+ * Program 11, Due: 11/12/19
+ * Partner names: None
+ * Description: 
+ */
+
+package jBorchers_Prog11;
+
+public class SammyRentals4
+{
+	// Do description 
+	public static void main (String[] args)
+	{
+		;
+	}
+}
