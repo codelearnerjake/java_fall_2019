8a56c2af3e8c96bf2307252727fe37d2b5c11294
Jacob Borchers <destinybound92@gmail.com>
2019-11-14 19:46:45 -0500

    Initiate array practice  (ArrayMultiples)

---
+++ JBorchers_Prog11/src/jBorchers_Prog12/ArrayMultiples.java
@@ -0,0 +1,66 @@
+/* Jacob Borchers
+ * Program 11, Due: 11/19/19
+ * Partner names: None
+ * Description: 
+ */
+
+package jBorchers_Prog12;
+import java.util.Scanner;
+
+public class ArrayMultiples
+{
+	public static void main (String[] args) 
+	{
+		printWelcome();
+	}
+	
+	Scanner userIn = new Scanner(System.in);
+	
+	/**
+	 * Prints welcome message and instructions for user
+	 */
+	public static void printWelcome() 
+	{
+		System.out.print("Enter numbers and a multiple and I will tell you if they "
+				+ "are a multiple of your number and if they are odd or even");
+	}
+	
+	/**
+	 * What does
+	 * 
+	 * @return
+	 */
+	public static int getTotal() 
+	{
+		System.out.print("How many numbers do you want to test?  ");
+		
+		int total = 0;
+		
+		return total;
+	}
+	
+	/**
+	 * What does 
+	 * 
+	 * @return
+	 
+	public static int getMultiple() 
+	{
+		
+	}
+	
+	/**
+	 * What does
+	 * 
+	 * @return
+
+	public static boolean isInvalid() 
+	{
+		
+	}
+	*/
+}
+
+
+
+
