0dc2eff1815f91017b7ce90723734afac02be5dc
Jacob Borchers <destinybound92@gmail.com>
2019-11-15 19:21:51 -0500

    Logic complete

--- JBorchers_Prog12/src/jBorchers_Prog12/ArrayMultiples.java
+++ JBorchers_Prog12/src/jBorchers_Prog12/ArrayMultiples.java
@@ -12,12 +12,52 @@ public class ArrayMultiples
 	public static void main (String[] args) 
 	{
 		printWelcome();
+
+		int amount = getTotal();	
 		
-		getTotal();
+		int[] testNums = new int[amount];
+		
+		for (int i= 0; i < amount; i++ )
+		{
+			System.out.print("Enter a number:  ");			
+						
+			testNums[i] = userIn.nextInt();
+		}
+		
+		int multiple = getMultiple();
+		
+		int count = 0;
+		for (int numMult : testNums)
+		{
+			if (numMult % multiple == 0)
+			{
+				System.out.printf("\n%d is a multple of %d",numMult, multiple);
+				count++;
+			}
+		}
+		
+		System.out.printf("\nThere were %d multiples of %d",count, multiple);
+		
+		for(int numPrint : testNums)
+		{
+			String evenOdd;
+			if (numPrint % 2 == 0)
+			{
+				evenOdd = "Even";
+			}
+			else 
+			{
+				evenOdd = "Odd";
+			}
+				
+			System.out.printf("\n %3d  %4s ", numPrint, evenOdd);
+		}
+		
+		
 		
 	}
 	
-	Scanner userIn = new Scanner(System.in);
+	static Scanner userIn = new Scanner(System.in);
 	
 	/**
 	 * Prints welcome message and instructions for user
@@ -35,31 +75,50 @@ public class ArrayMultiples
 	 */
 	public static int getTotal() 
 	{
-		System.out.print("How many numbers do you want to test?  ");
+		System.out.print("\nHow many numbers do you want to test?  ");
 		
-		int total = 0;
+		int testAmount = 0;
+		do
+		{
+			testAmount = userIn.nextInt();
+			if (!isInvalid(testAmount))
+			{
+				System.out.print("That is not greater than 0, try again: ");
+			}
+		}while(!isInvalid(testAmount));
 		
-		return total;
+		return testAmount;
 	}
 	
 	/**
 	 * What does 
 	 * 
 	 * @return
-	 
+	 */
 	public static int getMultiple() 
 	{
+		System.out.print("What number would you like to use for a multiple?  ");
 		
+		int num = userIn.nextInt();
+		
+		return num;
 	}
 	
 	/**
 	 * What does
 	 * 
 	 * @return
-
-	public static boolean isInvalid() 
+	 */
+	public static boolean isInvalid(int numIn) 
 	{
+		boolean validate = false;
+				
+		if (numIn > 0 )
+		{
+			validate = true; 
+		}
 		
+		return validate;
 	}
-	*/
+	
 }
