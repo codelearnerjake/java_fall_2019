9f6b94bb05ea008a7b5efa8d4fd78a6a205ee08a
Jacob Borchers <destinybound92@gmail.com>
2019-11-20 18:48:18 -0500

    First commit of Array practice 2 (Sales Report)
    Creating methods, adding driver class

---
+++ JBorchers_Prog13/src/jBorchers_Prog13/SalesReport.java
@@ -0,0 +1,97 @@
+/* Jacob Borchers
+ * Program 13, Due: 11/26/19
+ * Partner names: None
+ * Description: 
+ */
+
+package jBorchers_Prog13;
+import java.util.Scanner;
+
+public class SalesReport
+{
+	//Do description and method javadocs
+	public static void main(String[] args)
+	{
+		printTitle();
+		
+		int money = userIn.nextInt();
+	}
+	
+	static Scanner userIn = new Scanner(System.in);
+	
+	/**
+	 * Prints report title
+	 */
+	public static void printTitle() 
+	{
+		System.out.print("Company Logistics Co. Sales Report");
+	}
+	
+	/**
+	 * D
+	 * 
+	 */
+	public static void getSales()
+	{
+		;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static boolean isInvalid()
+	{
+		;
+	}
+	
+	/**
+	 * d
+	 */
+	public static void printSales()
+	{
+		;
+	}
+	
+	/**
+	 * d
+	 * 
+	 * @return
+	 */
+	public static int calculateHighest()
+	{
+		;
+	}
+	
+	/**
+	 * d
+	 * 
+	 * @return
+	 */
+	public static int calculateLowest()
+	{
+		;
+	}
+	
+	/**
+	 * d
+	 * 
+	 * @return
+	 */
+	public static double findAverage()
+	{
+		;
+	}
+	
+	/**
+	 * Will calculate the total of the sales and return that amount
+	 * 
+	 * @return
+	 */
+	public static double findTotal()
+	{
+		;
+	}	
+}
+
+
